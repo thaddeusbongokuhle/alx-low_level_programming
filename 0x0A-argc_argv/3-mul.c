b847210feef77105444a8d5dcbb012bc5293ef85
Bongokuhle <thaddeusbongokuhle@gmail.com>
2023-11-21 01:15:28 -0800

    3-mul.c

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * -atoi - method to convert string to an integer
+ *  @s: input string
+ *  Return: integer
+ */
+
+int _atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit =- digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
+/**
+ * main - program that multiplies two numbers
+ * @argc: input arguments
+ * @argv: input array
+ * Return: a zero
+ */
+
+int main(int argc, char *argv[])
+{
+	int result, n1, n2;
+
+	if (argc < 3 || argc > 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = _atoi(argv[1]);
+	n2 = _atoi(argv[2]);
+	result = n1 * n2;
+	printf("%d\n", result);
+
+	return (0);
+}
